6363cdb9a995b602c424e7eebd09a6e386ce5a8b
eLecCap1taL <166891662+eLecCap1taL@users.noreply.github.com>
2024-09-16 00:24:42 +0800

    On branch main

---
+++ Addon/JMove/AutoStop/AutoTickerGen.cpp
@@ -0,0 +1,41 @@
+//%^~
+#include <bits/stdc++.h>
+#define INF 0x7fffffff
+#define MAXN 100005
+#define eps 1e-9
+#define foru(a,b,c)	for(int a=b;a<=c;a++)
+#define RT return 0;
+#define LL long long
+#define LXF int
+#define RIN rin()
+#define HH printf("\n")
+#define All(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+using namespace std;
+inline LXF rin(){
+	LXF x=0,w=1;
+	char ch=0;
+	while(ch<'0'||ch>'9'){ 
+	if(ch=='-') w=-1;
+	ch=getchar();
+	}
+	while(ch>='0'&&ch<='9'){
+	x=x*10+(ch-'0');
+	ch=getchar();
+	}
+	return x*w;
+}
+signed main(){
+	foru(i,1,20){
+		freopen(("AutoStopTicker"+to_string(i)+".cfg").c_str(),"w",stdout);
+		string s="astk"+to_string(i);
+		cout<<(s+"_begin")<<'\n';
+		string out[2]={s+"\n","sleep 10\n"};
+		for(int i=2;i<=120001;i++){
+			cout<<out[i&1];
+		}
+//		cout<<("echo done "+s);
+	}
+	return 0;
+}
